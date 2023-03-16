5e96eff020b872bf0947460c2106d075c584ad74
Rahul Kumar <rahulrk00297@gmail.com>
2023-03-17 00:08:52 +0530

    Add project files.

---
+++ ImageUpload/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace ImageUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if(file== null)
+            {
+                return View();
+            }
+            else
+            {
+                string path = Server.MapPath("~/Img/Image");
+                string fileName = path.GetFileName(file.FileName);
+                string fullPath = path.combine(path, fileName);
+                file.SaveAs(fullPath);
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
